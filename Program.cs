5db3d0caa4a94497b57330227258740398ecfea0
Sofia Mayumi <mayuminakamaru1002@gmail.com>
2024-05-08 16:02:13 -0300

    feito todos os todo

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Nokia nokia = new Nokia("123456","nokia234","123456",521);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("whatsApp");
+
+
+Iphone iphone = new Iphone ("1234567","iphone 15","09382201910",234);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Instagram");
